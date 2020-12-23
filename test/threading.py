7da00a864eb61b7735d94d5a2e3f27f98c106173
Dave-The-IT-Guy <davevanderleek@gmail.com>
2020-12-23 13:52:53 +0100

    Template voor multithreading

--- test/threading.py
+++ test/threading.py
@@ -1,7 +1,3 @@
-# Python program killing
-# threads using stop
-# flag
-
 import threading
 import time
 
@@ -22,8 +18,6 @@ t1 = threading.Thread(target=run, args=(lambda: stop_threads,))
 
 t1.start()
 time.sleep(2)
-
-
 stop_threads = True
 t1.join()
 print('thread killed')
