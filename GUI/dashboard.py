b5fcc9d332d91b8ba66d3522405bac2933d139a8
Sokkeneter <74532462+Sokkeneter@users.noreply.github.com>
2020-12-27 15:24:18 +0100

    dashboard v1.2
    decided the sidebar was trivial n annoying so i yeeted it. the widgets can now be found in the second notebook page

--- GUI/dashboard.py
+++ GUI/dashboard.py
@@ -15,8 +15,11 @@ from tkcolorpicker import askcolor
 # to do
 # do a binry search for getting the details
 # make graph blue
-#slider in TI (0-64)
 # make it so that TI frame gets placed over GUI
+# if i search for someting it undoes the sorting go fix that #edit: fixed it but i havent tested if it works with evertything
+# add a filtering functionality
+# the sort functions give differing lists containing games that dont appear in the original list
+
 
 # -- globals
 global game_names
@@ -24,6 +27,8 @@ game_names = []
 global case_sensitive
 case_sensitive = True
 global sorting
+global sortedgames
+sortedgames = game_names
 
 # -- functions
 def openReadme():
@@ -93,13 +98,23 @@ def getDetails(i):
     selected = gameslist.get(gameslist.curselection()) # get the current selection in the listbox
     details.config(state=NORMAL) # set state to normal so that changes can be made to the textbox
     details.delete('1.0', END) #clear whatevers currently in the textbox
-    for game in json_naar_dict():
-        if game["name"] == selected :#too lazy for a binary search, it works fine :~)
+
+    sorted_dict = sorted(json_naar_dict(), key=lambda k: k['name'])   # sort list of dicts
+    start = 0  # yes im going to try and implement a  binary search and im in hell
+    end = len(sorted_dict) - 1
+    while start <= end:
+        middle = (start + end)// 2
+        game = sorted_dict[middle]
+        if game["name"] > selected:
+            end = middle - 1
+        elif game["name"] < selected:
+            start = middle + 1
+        else:
             details.insert(END, f'{game["name"]}\n')
-            details.insert(END, '_____________________________\n\n')
-            details.insert(END,f'release date:{game["release_date"]}\n')
-            details.insert(END,f'developer: {game["developer"]}\n')
-            details.insert(END,f'price: {game["price"]}\n')
+            details.insert(END, '_____________________________\n\n')  # this ones just for looks
+            details.insert(END, f'release date:{game["release_date"]}\n')
+            details.insert(END, f'developer: {game["developer"]}\n')
+            details.insert(END, f'price: {game["price"]}\n')
             details.insert(END, f'genres: {game["genres"]}\n')
             details.insert(END, f'platforms: {game["platforms"]}\n')
             details.insert(END, f'positive ratings: {game["positive_ratings"]}\n')
@@ -108,8 +123,14 @@ def getDetails(i):
             details.insert(END, f'owners: {game["owners"]} copies\n')
 
             details.config(state=DISABLED)  # set it back to disabled to the user cant write 'penis' in the textbox
-            return None
-    print("no matches found")
+            return None  #python gets mad at me if i dont return anything and i dont know why
+    # details.insert(END, f'no details available for {selected}')
+    # details.config(state=DISABLED)
+    # place i got the code from: https://stackoverflow.com/questions/34327244/binary-search-through-strings
+
+
+
+
 
 
 
@@ -121,6 +142,7 @@ def openSortAndFilterWindow():
     sorting = StringVar()
     settingswindow = Toplevel(master=root)
     sortframe = Frame(master=settingswindow)
+    settingswindow.wm_attributes("-topmost", 1)
     sortframe.grid(row=0, column=0)
 
     # --wigdets in window
@@ -142,16 +164,6 @@ def openSortAndFilterWindow():
 
 
 
-
-def toggleSidebar():#i havent put anything in here yet
-    if sidebar_frame.frame_status:
-        sidebar.grid_forget()
-        sidebar_frame.frame_status = False
-    else:
-        sidebar.grid(column=0, row=0, sticky="nesw")
-        sidebar_frame.frame_status = True
-
-
 def listInsert(list):
     for item in list:
         gameslist.insert(END, item)
@@ -161,14 +173,14 @@ def search(a):
 
     gameslist.delete(0, END)  # clear listbox
     if query == "":#if searchbar is empty, insert entire list
-        listInsert(game_names)
+        listInsert(sortedgames)
 
     if case_sensitive == True:
-        for game in game_names:
+        for game in sortedgames:
             if query in game:
                 gameslist.insert("end", game)
     else:
-        for game in game_names:
+        for game in sortedgames:
             no_case = game.lower()
             if query.lower() in no_case:
                 gameslist.insert("end", game)
@@ -192,21 +204,27 @@ def sortByNone():
 
 def sortByName():
     gameslist.delete(0, END)
+    global sortedgames
     sortedgames = sorteren("1") #get the sorted list
     listInsert(sortedgames)#and put it in the listbox
     current_sort_label.config(text=f"sorted by: name")
 
 def sortByPrice():
     gameslist.delete(0, END)
+    global sortedgames
     sortedgames = sorteren("2")
     listInsert(sortedgames)
     current_sort_label.config(text=f"sorted by: price")
 
 def sortByDate():
     gameslist.delete(0, END)
+    global sortedgames
     sortedgames = sorteren("3")
     listInsert(sortedgames)
     current_sort_label.config(text=f"sorted by: release date")
+    current_sort = "date"
+
+# def filterBy():
 
 def showgraph():
     t = [1, 2, 3, 4, 5, 6]
@@ -254,7 +272,7 @@ rightframe.grid(row=0,column=0, padx=10, pady=10)
 listframe = Frame(master=rightframe, bg="#0B3545")
 searchbarframe= Frame(master=rightframe,bg="#042430")
 searchbar = Entry(master=searchbarframe)
-searchbar.config(bg="#133d4d")
+searchbar.config(bg="#133d4d", fg="white", insertbackground="white", insertwidth=1) #look i just *do not* like the way the cursors in tkinter looks
 searchbar.bind("<Return>", search)
 search_label = Label(master=searchbarframe, text="search:", fg="white", bg="#042430")
 scrollbar = Scrollbar(listframe, orient="vertical")
@@ -290,13 +308,31 @@ leftframe.grid(row=0,column=1, padx=10, pady=10)
 leftframe_notebook = ttk.Notebook(leftframe)
 # leftframe_notebook.config(background="#042430")
 leftframe1 = ttk.Frame(leftframe_notebook)   # first page, which would get widgets gridded into it
-leftframe2 = ttk.Frame(leftframe_notebook)   # second page
+rpi_frame = ttk.Frame(leftframe_notebook)   # second page
 leftframe_notebook.add(leftframe1, text='graph')
-leftframe_notebook.add(leftframe2, text='Two')
-#
+leftframe_notebook.add(rpi_frame, text='raspberry pi')
+
+#rpi_frame
+rpilabel = Label(master=rpi_frame,text="                       raspberry pi functions", fg="white", bg="#0B3545")
+rpilabel.grid(row=0, padx=10, pady=10)
+TI_button1 = Button(master=rpi_frame, text="knop 1",bg="#042430",fg="white")
+TI_button1.grid(row= 1, padx=10, pady=10)
+TI_button2 = Button(master=rpi_frame, text="knop 2", bg="#042430",fg="white")
+TI_button2.grid(row=1, column=1)
+neopixel_label = Label(master=rpi_frame,text="                      neopixel functions", fg="white", bg="#0B3545")
+neopixel_label.grid(row=3)
+neopixel_options = ('off', 'white', 'pick color')
+current_neopxl = StringVar()
+current_neopxl.set(neopixel_options[0])
+TI_neopixel_options = OptionMenu(rpi_frame, current_neopxl, *neopixel_options, command=neopixelChange)
+TI_neopixel_options["menu"].config(bg="#042430", fg="white")
+TI_neopixel_options.grid(row=4)
+TI_slider = Scale(master=rpi_frame, from_=0, to=64, tickinterval=64,background="#0B3545", fg='white', troughcolor='#3D6D7F', activebackground='#09192A', highlightthickness=0)
+TI_slider.grid(row=4, column=1,padx=10, pady=10)
+
+
 #Notebook Style
 noteStyler = ttk.Style()
-
 
 # Import the Notebook.tab element from the default theme
 noteStyler.element_create('Plain.Notebook.tab', "from", 'default')
@@ -315,13 +351,11 @@ noteStyler.configure("TNotebook.Tab", background="#0B3545", foreground="white", 
 noteStyler.configure("TFrame", background="#0B3545", foreground="white")
 # bron: https://stackoverflow.com/questions/22389198/ttk-styling-tnotebook-tab-background-and-borderwidth-not-working
 
-#
 leftframe_notebook.grid()
 
 #--buttons
 sortbutton = Button(master=rightframe, text="settings", command=openSortAndFilterWindow)
 sortbutton.pack()
-
 
 #--menu
 menubar = Menu(root)
@@ -329,34 +363,6 @@ helpmenu = Menu(menubar, tearoff=0)
 menubar.add_cascade(label="Help", menu=helpmenu)
 helpmenu.add_command(label="Readme", command=openReadme)
 root.config(menu=menubar)
-
-
-
-
-#--sidebar
-sidebar_frame = Frame(root)
-sidebar_frame.grid(column=2, row=0)
-sidebar = Frame(sidebar_frame, bg='#0B3545', relief='sunken')
-sidebar_frame.frame_status = False
-img = PhotoImage(file = "open_friendlist_button.gif")
-sidebar_button = Button(sidebar_frame, image=img, command=toggleSidebar, height=450, relief='raised')
-sidebar_button.grid(row=0, column=1, sticky='nswe')
-
-    # shit inside the sidebar
-sidebarlabel = Label(master=sidebar,text="                       raspberry pi functions", fg="white", bg="#0B3545")
-sidebarlabel.grid(row=0, padx=10, pady=10)
-TI_button1 = Button(master=sidebar, text="button 1",bg="#042430",fg="white")
-TI_button1.grid(row= 1, padx=10, pady=10)
-TI_button2 = Button(master=sidebar, text="button 2", bg="#042430",fg="white")
-TI_button2.grid(row=1, column=1)
-neopixel_options = ('off', 'white', 'pick color')
-current_neopxl = StringVar()
-current_neopxl.set(neopixel_options[0])
-TI_neopixel_options = OptionMenu(sidebar, current_neopxl, *neopixel_options, command=neopixelChange)
-TI_neopixel_options["menu"].config(bg="#042430", fg="white")
-TI_neopixel_options.grid(row=2)
-TI_slider = Scale(master=sidebar, from_=0, to=64)
-TI_slider.grid(row=2, column=1,padx=10, pady=10)
 
 
 
